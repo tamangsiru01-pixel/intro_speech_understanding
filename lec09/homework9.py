c5955567ddd9f7e83e2336a671fcf134c19ad5d0
tamangsiru01-pixel <tamang.siru01@gmail.com>
2026-07-26 00:07:54 +0545

    Finish homework 9 voice activity detection functions

--- lec09/homework9.py
+++ lec09/homework9.py
@@ -13,7 +13,15 @@ def VAD(waveform, Fs):
     segments (list of arrays) - list of the waveform segments where energy is 
        greater than 10% of maximum energy
     '''
-    raise RuntimeError("You need to change this part")
+    framelength = int(0.025*Fs)
+    frameskip = int(0.01*Fs)
+    frames = np.array([waveform[m:m+framelength] for m in range(0, len(waveform)-framelength, frameskip)])
+    energy = np.sum(np.square(frames), axis=1)
+    vad = np.array([1 if energy[m] > 0.1*max(energy) else 0 for m in range(len(energy))])
+    start_times = [m*frameskip for m in range(1, len(vad)) if vad[m-1]==0 and vad[m]==1]
+    end_times = [m*frameskip for m in range(1, len(vad)) if vad[m-1]==1 and vad[m]==0]
+    segments = [waveform[start_times[k]:end_times[k]] for k in range(len(start_times))]
+    return segments
 
 def segments_to_models(segments, Fs):
     '''
@@ -29,7 +37,15 @@ def segments_to_models(segments, Fs):
     @returns:
     models (list of arrays) - average log spectra of pre-emphasized waveform segments
     '''
-    raise RuntimeError("You need to change this part")
+    N = int(0.004*Fs)
+    S = int(0.002*Fs)
+    models = []
+    for x in segments:
+        frames = np.array([x[m+1:m+1+N]-x[m:m+N] for m in range(0, len(x)-N, S)])
+        mstft = np.abs(np.fft.fft(frames, axis=1))
+        sgram = 20*np.log10(np.maximum(0.01*np.amax(mstft), mstft))
+        models.append(np.average(sgram[:, 0:int(N/2)], axis=0))
+    return models
 
 def recognize_speech(testspeech, Fs, models, labels):
     '''
@@ -47,6 +63,15 @@ def recognize_speech(testspeech, Fs, models, labels):
     sims (Y-by-K array) - cosine similarity of each model to each test segment
     test_outputs (list of strings) - recognized label of each test segment
     '''
-    raise RuntimeError("You need to change this part")
+    testsegments = VAD(testspeech, Fs)
+    testspectra = segments_to_models(testsegments, Fs)
+    sims = np.zeros((len(models), len(testspectra)))
+    for y in range(len(models)):
+        for k in range(len(testspectra)):
+            x = testspectra[k]
+            m = models[y]
+            sims[y,k] = np.dot(x,m) / (np.sqrt(np.sum(np.square(x)))*np.sqrt(np.sum(np.square(m))))
+    test_outputs = [labels[np.argmax(sims[:,k])] for k in range(len(testspectra))]
+    return sims, test_outputs
 
 
